282eedd32be25de4caa4d971add9046f45b5d5b2
nj-eka <njeka0108@gmail.com>
2022-11-20 03:49:09 +0300

    add calendar to bot

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,12 @@
 import os
+from datetime import date
 import telebot as tb
 from telebot import types
 import asyncio
+import logging
+
+from app.keyboards import generate_calendar_days, generate_calendar_months, EMTPY_FIELD
+from app.filters import calendar_factory, calendar_zoom, bind_filters
 
 bot = tb.TeleBot(os.environ.get("TBOT_TOKEN"), parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
@@ -144,3 +149,35 @@ def send_shipment(message):
 def function_name(message):
     print(message.location)
     bot.reply_to(message, 'Thank you!')
+
+
+# calendar
+
+@bot.message_handler(commands='calendar')
+def calendar_command_handler(message: types.Message):
+    now = date.today()
+    bot.send_message(message.chat.id, 'Calendar', reply_markup=generate_calendar_days(year=now.year, month=now.month))
+
+
+@bot.callback_query_handler(func=None, calendar_config=calendar_factory.filter())
+def calendar_action_handler(call: types.CallbackQuery):
+    callback_data: dict = calendar_factory.parse(callback_data=call.data)
+    year, month = int(callback_data['year']), int(callback_data['month'])
+    logging.ingo('calandar: %n, %n', year, month)
+
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.id,
+                                  reply_markup=generate_calendar_days(year=year, month=month))
+
+
+@bot.callback_query_handler(func=None, calendar_zoom_config=calendar_zoom.filter())
+def calendar_zoom_out_handler(call: types.CallbackQuery):
+    callback_data: dict = calendar_zoom.parse(callback_data=call.data)
+    year = int(callback_data.get('year'))
+
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.id,
+                                  reply_markup=generate_calendar_months(year=year))
+
+
+@bot.callback_query_handler(func=lambda call: call.data == EMTPY_FIELD)
+def callback_empty_field_handler(call: types.CallbackQuery):
+    bot.answer_callback_query(call.id)
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime
 from pathlib import Path
 
 from config import *
-from app import bot
+from app import bot, filters
 
 if __name__ == '__main__':
     args = None
@@ -29,7 +29,9 @@ if __name__ == '__main__':
         logger.info('tbot started with args: %s', args)
         logger.info('tbot_token: %s', os.environ.get("TBOT_TOKEN"))
 
+        filters.bind_filters(bot.bot)
         bot.bot.infinity_polling()
+
     except KeyboardInterrupt:
         logging.info('tbot is stopped by user.')
     except BaseException as err:
